f50295f0de71e7f72b4e0fa62f2aff8b1cb40dd7
hrivascortes <hrivas.cortes@gmail.com>
2020-04-18 23:38:31 -0500

    1804201750

--- Uniprotec/inicio/src/main/resources/static/js/complemento.js
+++ Uniprotec/inicio/src/main/resources/static/js/complemento.js
@@ -27,26 +27,21 @@
 	  
 $(document).ready(function(){
 	
-	/*
-	 * Variables Globales
-	 */
-
-	
+		
 	/*
 	 * Carga la Tabla inicial
 	 */
+	
 	window.operateEventsUpdate = {
 		    'click .like': function (e, value, row, index) {
 //		      alert('You click like action, row: ' + row.idUserEmpresa);//JSON.stringify(row))
-
 		      $('#nombreCortoCliente').val(row.nombreCortoCliente);
 		      $('#nombreCompletoCliente').val(row.nombreCompletoCliente);
 		      $('#emailCliente').val(row.emailCliente);
 		      $('#telefonoCliente').val(row.telefonoCliente);
 		      $('#rfcCliente').val(row.rfcCliente);
 		      $('#idRegionCliente').val(row.idRegionCliente);
-		      $('#domicilioCliente').val(row.domicilioCliente);
-		      
+		      $('#domicilioCliente').val(row.domicilioCliente);      
 		    }
 		   }
 		  
@@ -55,35 +50,24 @@ $(document).ready(function(){
 	    'click .remove': function (e, value, row, index) {
 	    	confirm("Estás seguro de Eliminar el Registro : "+row.nombreRegistro);
 	    	$('#clientesTable').bootstrapTable('remove', {
-	        field: 'idUserEmpresa',
-	        values: [row.idUserEmpresa]
+	        field: 'idCliente',
+	        values: [row.idCliente]
 	      });
 	      console.log(row);
 	      deleteUserEmpresa(row.idUserEmpresa);
 	    }
 	  }
+
+	$('#clientesTable').bootstrapTable({data : $data})
+//		$table.bootstrapTable('load', data)
+	
+	
 	
 	
 	
 	/*
 	 *  funciones
 	 */
-	var $table = $('#clientesTable')
-	var $button = $('#selectId')
-
-	  
-	 $('.selectId').click(function () {
-	      alert('getSelections: ' + JSON.stringify($table.bootstrapTable('getSelections')));
-	      alert('jsonCompleto: ' + username);
-//	      alert('jsonIndividual: ' + cliente.nombreCortoCliente);
-	    });
-		  
-	  
-	    var $data = clientes
-//	    console.log(JSON.stringify(clientes));
-//	    console.log($data);
-	    $table.bootstrapTable({data: $data});
-//	    $('#clientesTable').bootstrapTable('load', clientes);
 	  
 
 
@@ -94,150 +78,10 @@ $(document).ready(function(){
 	$('#btnSaveRegistro').click(function(){
 		limpiaAlerta();
 		console.log($('#idUserEmpresa').val());
-		if($('#idUserEmpresa').val()==null || $('#idUserEmpresa').val()==''){
-		valoresRegistro = $('#nombreRegistro').val()+"++"+$('#apellidoRegistro').val()+"++"+$('#emailRegistro').val()+"++"+$('#telefonoRegistro').val()+"++"+$('#usuarioRegistro').val()+"++"+$('#passRegistro1').val()+"++"+$('#messageRegistro').val();
-		 
-			registroJson = { action : $.action,
-				 idEmpresa : $.idEmpresa,	
-//				 seccion : "bodySeccionArray1",
-				 valoresFinales : valoresRegistro}
-			console.log(registroJson);
 
-		$.ajax({
-		   	  url: url +"createUserEmpresa/",//+ context,//+finalJson.action+"/"+finalJson[1],
-		      dataType: 'json',
-			  type: 'POST',
-			  contentType: "application/json",
-			  data: JSON.stringify(registroJson),
-			  headers: {  'Access-Control-Allow-Origin': url, 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS', 'Access-Control-Allow-Headers': 'X-PINGOTHER' },
-			  crossDomain: true,
-			  success: 	function(data){					  
-				  console.log(data);
-				  avisaAlerta(data);
-				  setTimeout(function() {
-					  readIdUserEmpresa(0);
-					}, 8000);
-				},
-			  error: function(){
-				  errorAlerta();
-			  }
-			});
-		}else{
-			
-			valoresRegistro = $('#idUserEmpresa').val()+"++"+$('#nombreRegistro').val()+"++"+$('#apellidoRegistro').val()+"++"+$('#emailRegistro').val()+"++"+$('#telefonoRegistro').val()+"++"+$('#usuarioRegistro').val()+"++"+$('#passRegistro1').val()+"++"+$('#messageRegistro').val();
-			 
-			registroJson = { action : $.action,
-				 idEmpresa : $.idEmpresa,	
-//				 seccion : "bodySeccionArray1",
-				 valoresFinales : valoresRegistro}
-			console.log(registroJson);
-
-		$.ajax({
-		   	  url: url +"updateUserEmpresa/",//+ context,//+finalJson.action+"/"+finalJson[1],
-		      dataType: 'json',
-			  type: 'POST',
-			  contentType: "application/json",
-			  data: JSON.stringify(registroJson),
-			  headers: {  'Access-Control-Allow-Origin': url, 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS', 'Access-Control-Allow-Headers': 'X-PINGOTHER' },
-			  crossDomain: true,
-			  success: 	function(data){					  
-				  console.log(data);
-				  avisaAlerta(data);
-				  setTimeout(function() {
-					  readIdUserEmpresa(0);
-					}, 8000);
-				},
-			  error: function(){
-				  errorAlerta();
-			  }
-			});
-
-			
-		}
 	});
 	
 	
-		function readIdUserEmpresa(idUserEmpresa){
-		limpiaAlerta();
-		valoresRegistro = $('#readidUserEmpresa').val();
-		$.idUserEmpresa = idUserEmpresa
-		 
-			registroJson = { idAction : $.action,
-							idUserEmpresa : $.idUserEmpresa,	
-				 }
-			console.log(registroJson);
-
-		$.ajax({
-		   	  url: url +"crud/clientes",
-		      dataType: 'json',
-			  type: 'GET',
-			  contentType: "application/json",
-//			  data: JSON.stringify(registroJson),
-			  headers: {  'Access-Control-Allow-Origin': url, 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS', 'Access-Control-Allow-Headers': 'X-PINGOTHER' },
-			  crossDomain: true,
-			  success: 	function(data){
-				  if(data.length > 0){
-    				  console.log(data);
-//	    			  var $table = $('#userEmpresaTable');
-		   			//$('#userEmpresaTable').bootstrapTable('load', $.userEmpresa);
-//					$('#userEmpresaTable').bootstrapTable({data : data})
-//					console.log("function load data option");
-					$('#userEmpresaTable').bootstrapTable('load', data);
-//					console.log("function load data metodo");
-					$('#userEmpresaTable').bootstrapTable('refresh');
-//					console.log("function refresh data");
-//					  console.log(data.length);
-				  }else{
-					  alert("Registros Usuarios: 0");
-					  console.log("No existen usuarios de empresa");
-				  }
-				},
-			  error: function(){
-				  errorAlerta();
-			  }
-			});
-	}
-		
-		function deleteUserEmpresa(idUserEmpresa){
-			
-			limpiaAlerta();
-			
-			$.idUserEmpresa = idUserEmpresa
-			registroJson = { action : $.action,
-								valoresFinales : $.idUserEmpresa,	
-					 }
-				console.log("deleteUSerEmpresa");
-				console.log(registroJson);
-
-			$.ajax({
-			   	  url: url +"deleteUserEmpresa",
-			      dataType: 'json',
-				  type: 'POST',
-				  contentType: "application/json",
-				  data: JSON.stringify(registroJson),
-				  headers: {  'Access-Control-Allow-Origin': url, 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS', 'Access-Control-Allow-Headers': 'X-PINGOTHER' },
-				  crossDomain: true,
-				  success: 	function(data){					  
-					  console.log(data);
-					  avisaAlerta(data);
-					  if(data.codigo === 0){
-						  console.log("Registro User Eliminado");
-							$('#userEmpresaTable').bootstrapTable('refresh');
-//						  location.reload();
-						  
-						  }else{
-							  alert("Registros Usuarios: 0");
-							  console.log("No existen usuarios de empresa");
-						  }
-					},
-				  error: function(){
-					  errorAlerta();
-				  }
-				});
-
-			
-			
-		}
 	
 	
 	
