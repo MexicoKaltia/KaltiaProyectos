600888968f62e7c5c9d6e651000e04b672eb3fa2
hrivascortes <hrivas.cortes@gmail.com>
2020-04-28 22:19:03 -0500

    280420200100

--- Uniprotec/inicio/src/main/java/mx/uniprotec/inicio/controller/ControllerCrud.java
+++ Uniprotec/inicio/src/main/java/mx/uniprotec/inicio/controller/ControllerCrud.java
@@ -1,10 +1,6 @@
 package mx.uniprotec.inicio.controller;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
